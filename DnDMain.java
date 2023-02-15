d4fb9b8acf28426ff358963b3ed6a582bcd350b0
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-02-16 00:12:20 +0100

    Classes
    Class setup begins

---
+++ DnDMain.java
@@ -0,0 +1,12 @@
+public class DnDMain {
+    public static void main(String[] args) {
+        Roles mage = new Roles(35,100,90,40);
+        Roles paladin = new Roles(65,200,40,120);
+        Roles rogue = new Roles(100,100,100,100);
+        Roles warlock = new Roles(45,120,70,60);
+        Roles bard = new Roles(40,100,80,40);
+        Roles barbarian = new Roles(70,150,50,80);
+        
+        System.out.println("You chose the Mage class,your stats are: \nStrength: "+mage.health+"\nHealth: "+mage.health+"\nIntelligence: "+mage.intelligence+"\nArmor: "+mage.armor);
+    }
+}
---
+++ Roles.java
@@ -0,0 +1,11 @@
+public class Roles {
+    
+    int strength,health,intelligence,armor;
+
+    Roles(int strength,int health,int intelligence,int armor){
+        this.strength=strength;
+        this.intelligence=intelligence;
+        this.armor=armor;
+        this.health=health;
+    }
+}
